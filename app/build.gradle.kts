85616e36c641b1e84950b32d19f10b61ff13da8d
Ankur Ghimire <ankurghi2341@gmail.com>
2026-02-03 10:24:13 +0545

    ag_room details page and room preferences added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
-    implementation(libs.play.services.maps)
+    implementation(libs.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
